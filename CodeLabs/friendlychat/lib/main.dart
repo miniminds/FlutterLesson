81bc89511b8600cad070564b1646d6eac8030203
kwanlai <kwan@miniminds.net>
2018-12-08 16:20:41 -0500

    Step5_inprogress

--- CodeLabs/friendlychat/lib/main.dart
+++ CodeLabs/friendlychat/lib/main.dart
@@ -14,11 +14,36 @@ class FriendlychatApp extends StatelessWidget {
   }
 }
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+@override
+State createState()=>new ChatScreenState();
+}
+
+class ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textEditingController=new  TextEditingController();
+Widget _buildTextComposer() {
+  return new Container(
+    margin: const EdgeInsets.symmetric(horizontal: 8.0),
+child: new TextField(
+  controller: _textEditingController,
+  onSubmitted: _handleSubmitted,
+  decoration: new InputDecoration.collapsed( 
+    hintText: "Send a message"
+  ),
+),
+  );
+}
+
+void _handleSubmitted(String text) {
+  _textEditingController.clear();
+}
+
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(title: new Text("FriendlyChat")),
+      body: _buildTextComposer(),
     );
   }
 }
