b5bd5d82e54fb0789cbac29fc6c93b8fe1d1fc28
kwanlai <kwan@miniminds.net>
2018-12-09 12:28:21 -0500

    Step5_Completed
    apply the themadara in the Material App that should apply to every icons in the app instead of at the IconTheme that  use in CodeLabs

--- CodeLabs/friendlychat/lib/main.dart
+++ CodeLabs/friendlychat/lib/main.dart
@@ -9,35 +9,50 @@ class FriendlychatApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: "Firendly chat",
+      theme: new ThemeData( accentColor: Colors.amberAccent),
       home: new ChatScreen(),
     );
   }
 }
 
 class ChatScreen extends StatefulWidget {
-@override
-State createState()=>new ChatScreenState();
+  @override
+  State createState() => new ChatScreenState();
 }
 
 class ChatScreenState extends State<ChatScreen> {
-  final TextEditingController _textEditingController=new  TextEditingController();
-Widget _buildTextComposer() {
-  return new Container(
-    margin: const EdgeInsets.symmetric(horizontal: 8.0),
-child: new TextField(
-  controller: _textEditingController,
-  onSubmitted: _handleSubmitted,
-  decoration: new InputDecoration.collapsed( 
-    hintText: "Send a message"
-  ),
-),
-  );
-}
+  final TextEditingController _textEditingController =
+      new TextEditingController();
 
-void _handleSubmitted(String text) {
-  _textEditingController.clear();
-}
+  Widget _buildTextComposer() {
+    return new Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: new Row(
+        children: <Widget>[
+          new Flexible(
+            child: new TextField(
+              controller: _textEditingController,
+              onSubmitted: _handleSubmitted,
+              decoration:
+                  new InputDecoration.collapsed(hintText: "Send a message"),
+            ),
+          ),
+          new Container(
+            margin:new EdgeInsets.symmetric(horizontal: 4.0),
+            child:new IconButton(
+              icon:  new Icon(Icons.send),
+              color: Theme.of(context).accentColor,
+              onPressed: () => _handleSubmitted(_textEditingController.text),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 
+  void _handleSubmitted(String text) {
+    _textEditingController.clear();
+  }
 
   @override
   Widget build(BuildContext context) {
