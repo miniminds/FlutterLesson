3792c60b86c71571b075095ccb0852cce5411e27
kwanlai <kwan@miniminds.net>
2018-12-11 14:11:48 -0500

    SperateWidget
    seperate ChatMessage in seperate file

--- CodeLabs/friendlychat/lib/main.dart
+++ CodeLabs/friendlychat/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:friendlychat/widgets/ChatMessageWidget.dart';
 
-const String _name = "James AA";
+
 void main() {
   runApp(new FriendlychatApp());
 }
@@ -110,40 +111,3 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
   }
 }
 
-class ChatMessage extends StatelessWidget {
-  ChatMessage({this.text, this.animationController});
-  final AnimationController animationController;
-  final String text;
-
-  @override
-  Widget build(BuildContext context) {
-    return new SizeTransition(
-        sizeFactor: new CurvedAnimation(
-            parent: animationController, curve: Curves.easeOut),
-        axisAlignment: 0.0,
-        child: new Container(
-          margin: const EdgeInsets.symmetric(vertical: 10.0),
-          child: new Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              new Container(
-                margin: const EdgeInsets.only(right: 16.0),
-                child: new CircleAvatar(child: new Text(_name[0])),
-              ),
-              new Expanded(
-                child: new Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    new Text(_name, style: Theme.of(context).textTheme.subhead),
-                    new Container(
-                      margin: const EdgeInsets.only(top: 5.0),
-                      child: new Text(text),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ));
-  }
-}
