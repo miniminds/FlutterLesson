ec3b6dd002ca207a0eceb17e26f631da1ae14a45
kwanlai <kwan@miniminds.net>
2018-12-20 16:27:34 -0500

    MDC102

--- CodeLabs/shrine/lib/login.dart
+++ CodeLabs/shrine/lib/login.dart
@@ -58,7 +58,9 @@ class _LoginPageState extends State<LoginPage> {
                 ),
                 RaisedButton(
                   child: Text("Next"),
-                  onPressed: () {},
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
                 ),
               ],
             ),
