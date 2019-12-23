2f2d74ec44096d829058f70a40cee111a97748ec
KL <kwan@miniminds.net>
2019-12-23 09:42:40 -0500

    micard_1222

--- flutter_bootcamp/mi_card_flutter/lib/main.dart
+++ flutter_bootcamp/mi_card_flutter/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -11,17 +13,52 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.teal,
           body: SafeArea(
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                Text('Image'),
-                Image(
-                  width: 150,
-                  height: 150,
-                  image: AssetImage('miCard.jpg'),
-                ),
                 CircleAvatar(
                   radius: 50.0,
-                  backgroundImage: AssetImage('assets/miCard.jpg'),
+                  backgroundImage: AssetImage('assets/images/miCard.jpg'),
                 ),
+                Text(
+                  'Alien',
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontFamily: 'Kaushan Script',
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ), Text(
+                  'SPACE NAVIGATOR ',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontFamily: 'Source Sans Pro',
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  height: 36.0,
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                      ),
+                      SizedBox(
+                        width: 12.0,
+                      ),
+                      Text(
+                        '1-999-111-1234',
+                        style: TextStyle(
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 18.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(),
               ],
             ),
           )),
