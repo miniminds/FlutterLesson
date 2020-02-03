bdb46dd52383f9af63ecfc1faa52364f631a4072
kwanlai <kwan@miniminds.net>
2020-02-03 09:29:28 -0500

    Dice0203

--- flutter_bootcamp/dicee_flutter/lib/main.dart
+++ flutter_bootcamp/dicee_flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,57 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    leftDiceNumber = Random().nextInt(5) + 1;
+                  });
+                },
+                child: Image.asset("images/dice$leftDiceNumber.png"),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: FlatButton(
+                  onPressed: () {
+                    setState(() {
+                      rightDiceNumber = Random().nextInt(5) + 1;
+                    });
+                  },
+                  child: Image.asset("images/dice$rightDiceNumber.png"),
+                )),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(leftDiceNumber.toString()  ),
+              Text(rightDiceNumber.toString()),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
