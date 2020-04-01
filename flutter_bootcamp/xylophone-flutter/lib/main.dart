49a1cc8388195d2382a8bb652f569d62c59d4e33
kwanlai <kwan@miniminds.net>
2020-04-01 08:57:00 -0400

    xnoolphone

---
+++ flutter_bootcamp/xylophone-flutter/lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                FlatButton(
+                    color: Colors.red,
+                    onPressed: () {
+                      playSound(7);
+                    },
+                    child: null,),
+                FlatButton(
+                  color: Colors.orange,
+                  onPressed: () {
+                    playSound(7);
+                  },
+                  child: null,
+                ),
+                FlatButton(
+                  color: Colors.yellow,
+                  onPressed: () {
+                     playSound(7);
+                  },
+                  child: null,
+                ),
+                FlatButton(
+                  color: Colors.lightGreen,
+                  onPressed: () {
+                    playSound(7);
+                  },
+                  child: null,
+                ),
+                FlatButton(
+                  color: Colors.green,
+                  onPressed: () {
+                     playSound(7);
+                  },
+                  child: null,
+                ),
+                FlatButton(
+                  color: Colors.blue,
+                  onPressed: () {
+                     playSound(7);
+                  },
+                  child: null,
+                ),
+                FlatButton(
+                  color: Colors.purple,
+                  onPressed: () {
+                    playSound(7);
+                  },
+                  child: null,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+  void playSound(int soundNumber)
+  {
+    final player=AudioCache();
+    player.play("note$soundNumber.wav");
+  }
+}
